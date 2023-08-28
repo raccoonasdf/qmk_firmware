11fc478d8676c6af9bd56499b38623dad5fdca32
raccoon! <raccoon@raccoon.fun>
2023-08-28 03:19:08 -0400

    Fix stuff

--- keyboards/preonic/keymaps/raccoonasdf/config.h
+++ keyboards/preonic/keymaps/raccoonasdf/config.h
@@ -2,9 +2,13 @@
 
 #define USB_POLLING_INTERVAL_MS 1
 
-#define TAP_CODE_DELAY 2
+// certain stinky programs dont register the modtap enter unless you slow it down
+#define TAP_CODE_DELAY 20
 
 #define RGBLIGHT_DEFAULT_HUE 180
+
+// startup song doesn't work anymore unless i set this?
+#define AUDIO_INIT_DELAY
 
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(Q__NOTE(_D4), Q__NOTE(_D4), Q__NOTE(_D5), Q__NOTE(_REST), Q__NOTE(_A4))
